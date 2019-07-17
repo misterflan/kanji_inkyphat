8de8ea914096163d0bad8b11e342292f5eae5163
misterflan <rich.cunningham@gmail.com>
2019-07-17 14:45:42 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # kanji_inkyphat
+
+## Introduction
+
+A simple python script scrape www.kanji-a-day.com using BeautifulSoup and display it on a inkypHAT
+
+## Installation
+
+`pip3 -i requirements.txt`
+and then add into crontab to run every few hours to keep it rolling through the kanji :)
+
+## Limitations
+
+I've got the red/black/white inkypHAT, need to add args to allow passing through of other inky's
--- kanji.py
+++ kanji.py
@@ -9,7 +9,6 @@ inky_display = InkyPHAT("red")
 
 # Scrape and get informationi
 urls = ['http://www.kanji-a-day.com/level1/index.php', 'http://www.kanji-a-day.com/level2/index.php', 'http://www.kanji-a-day.com/level3/index.php', 'http://www.kanji-a-day.com/level4/index.php']
-
 res = requests.get(choice(urls))
 soup = BeautifulSoup(res.content, "lxml")
 kanji = soup.find("div", {"class":"glyph"})
@@ -26,7 +25,7 @@ ka = kanji.contents[0]
 # Select Hirigana
 ku = ((kun.contents[3].text).strip().split('\xa0')[0])
 
-# Define fonts
+# Define fonts and sizes
 kanjifont=ImageFont.truetype("corp_round_v1.ttf",50)
 unicodefont=ImageFont.truetype("corp_round_v1.ttf",30)
 normal=ImageFont.truetype(FredokaOne,22)
@@ -38,15 +37,16 @@ draw = ImageDraw.Draw(image)
 y_top = int(inky_display.HEIGHT * (5.0 / 10.0))
 y_bottom = y_top + int(inky_display.HEIGHT * (4.0 / 10.0))
 
-# Draw the red, white, and red strips
-
+# Paint it black
 for y in range(0, inky_display.height):
     for x in range(0, inky_display.width):
         image.putpixel((x, y), inky_display.BLACK)
 
+# Write the text
 draw.text((100,-30), ka, inky_display.RED, font=kanjifont)
 draw.text((10,15), ku, inky_display.RED, font=unicodefont)
 draw.text((10,45), pmean, inky_display.WHITE, font=normal)
 
+# Display the text!
 inky_display.set_image(image)
 inky_display.show()
